8b024074fad0b13e07f63b66c63bc30054f27ff6
Milan Chaulagain <mchaulagain57@gmail.com>
2025-05-02 10:13:26 +0545

    Add mongoDB and mongoose to save user data from clerk

---
+++ src/lib/mongodb/mongoose.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let isConnected = false; // Track the connection status
+
+export const connect = async()=> {
+    mongoose.set("strictQuery", true); // Set strictQuery to true
+    if (isConnected) {
+        console.log("MongoDB is already connected");
+        return;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "next auth app",
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        // Check if the connection is successful
+        console.log("MongoDB connected successfully");
+        isConnected = true; // Update the connection status
+
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+    }
+}
